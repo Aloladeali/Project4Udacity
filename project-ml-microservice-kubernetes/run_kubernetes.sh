2302781bda98729095a95319b9dd9b8f18b2ba1b
Alolade <aloladeali@gmail.com>
2022-08-04 15:15:14 +0000

    Checking connection

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="alolade/projectfour:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run projectfour --image=alolade/projectfour:v1.0.0 --port=80 --labels app=projectfour
 
 
 # Step 3:
-# List kubernetes pods
+# List kubernetes 
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward projectfour 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="alolade/projectfour:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag projectfour $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
